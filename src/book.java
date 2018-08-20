3764b8276d7bd51772ed91d06a01ba422917a906
Testers11 <41605926+Testers11@users.noreply.github.com>
2018-08-20 15:20:43 +1000

    Changing cycle 1
    have done the changes from 1.01 to 1.14

--- src/book.java
+++ src/book.java
@@ -2,31 +2,31 @@ import java.io.Serializable;
 
 
 @SuppressWarnings("serial")
-public class Book implements Serializable { //class name should be in cAPITAL
+public class Book implements Serializable { // change_1.01 class name book has changes to Book
 	
-	private String T; //test
-	private String A;
-	private String C;
-	private int ID;
-	//nashath
+	private String title; // change_1.02 veriable name T has changed to title
+	private String author; // change_1.03 veriable name A has changed to author
+	private String callNo; // change_1.04 veriable name C has changed to callNo
+	private int id; // change_1.05 veriable name ID has changed to id
+	
 	private enum STATE { AVAILABLE, ON_LOAN, DAMAGED, RESERVED };
 	private STATE state;
 	
 	
-	public Book(String author, String title, String callNo, int id) {  //class name should be in cAPITAL
-		this.A = author;
-		this.T = title;
-		this.C = callNo;
-		this.ID = id;
+	public Book(String author, String title, String callNo, int id) {  // change_1.06 class name book has changed to Book
+		this.author = author; // change_1.07 this.A has changed to this.author
+		this.title = title; // change_1.08 this.T has changed to this.title
+		this.callNo = callNo; // change_1.09 this.C has changed to this.callNo
+		this.id = id; // change_1.10 this.ID has changed to this.id
 		this.state = STATE.AVAILABLE;
 	}
 	
 	public String toString() {
 		StringBuilder sb = new StringBuilder();
-		sb.append("Book: ").append(ID).append("\n")
-		  .append("  Title:  ").append(T).append("\n")
-		  .append("  Author: ").append(A).append("\n")
-		  .append("  CallNo: ").append(C).append("\n")
+		sb.append("Book: ").append(id).append("\n") // change_1.11 ID has changed to id.
+		  .append("  Title:  ").append(title).append("\n") // change_1.12 T has changed to title.
+		  .append("  Author: ").append(author).append("\n") // change_1.13 A has changed to author.
+		  .append("  CallNo: ").append(C).append("\n") // change_1.14 C has changed to callNo.
 		  .append("  State:  ").append(state);
 		
 		return sb.toString();
