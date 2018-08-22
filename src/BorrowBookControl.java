2056074603aeca10cc6f17ab113785f4f63fd65a
Testers11 <41605926+Testers11@users.noreply.github.com>
2018-08-23 01:40:23 +1000

    BorrowBookControl.java class changes cycle 1
    changes from 1.01 to 1.08 have done

--- src/BorrowBookControl.java
+++ src/BorrowBookControl.java
@@ -1,22 +1,22 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class BorrowBookControl { // hello
+public class BorrowBookControl {
 	
 	private BorrowBookUI ui;
 	
-	private library L;
-	private member M;
+	private Library library; //change_1.01 Object class name library has changed to Library and object reference variable name L has changed to library
+	private Member member; //change_1.02 Object class name member has changed to Member and object reference variable name M has changed to member
 	private enum CONTROL_STATE { INITIALISED, READY, RESTRICTED, SCANNING, IDENTIFIED, FINALISING, COMPLETED, CANCELLED };
 	private CONTROL_STATE state;
 	
 	private List<book> PENDING;
 	private List<loan> COMPLETED;
-	private book B;
+	private Book book; // change_1.03 Object class name book has changed to Book and object reference variable name B has changed to book
 	
 	
 	public BorrowBookControl() {
-		this.L = L.INSTANCE();
+		this.library = library.INSTANCE(); // change_1.04 variable name L has changed to library as defined above. 
 		state = CONTROL_STATE.INITIALISED;
 	}
 	
@@ -31,16 +31,16 @@ public class BorrowBookControl { // hello
 	}
 
 		
-	public void Swiped(int memberId) {
+	public void swiped(int memberId) { // change_1.05 method name Swiped has changed to swipe as first letter should be lowercase
 		if (!state.equals(CONTROL_STATE.READY)) 
 			throw new RuntimeException("BorrowBookControl: cannot call cardSwiped except in READY state");
 			
-		M = L.getMember(memberId);
-		if (M == null) {
+		member = library.getMember(memberId); // change_1.06 variable names M and L have changed to member and library as defined above. 
+		if (member == null) { // change_1.07 variable name M has changed to member.
 			ui.display("Invalid memberId");
 			return;
 		}
-		if (L.memberCanBorrow(M)) {
+		if (library.memberCanBorrow(member)) { // change_1.08 variable names L and M have changed to library and member as defined above.
 			PENDING = new ArrayList<>();
 			ui.setState(BorrowBookUI.UI_STATE.SCANNING);
 			state = CONTROL_STATE.SCANNING; }
@@ -48,8 +48,7 @@ public class BorrowBookControl { // hello
 		{
 			ui.display("Member cannot borrow at this time");
 			ui.setState(BorrowBookUI.UI_STATE.RESTRICTED); }}
-	
-	
+
 	public void Scanned(int bookId) {
 		B = null;
 		if (!state.equals(CONTROL_STATE.SCANNING)) {
