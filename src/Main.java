4bb41b4e669bb74dad79048ff576f9a170791aaf
hashini <hashiniuttara@ymail.com>
2018-08-25 16:24:01 +1000

    Change Cycle 1 - main.java

--- src/Main.java
+++ src/Main.java
@@ -4,17 +4,17 @@ import java.util.Scanner;
 
 public class Main {
 	
-	private static Scanner IN;
-	private static library LIB;
-	private static String MENU;
-	private static Calendar CAL;
-	private static SimpleDateFormat SDF;
+	private static Scanner inputData;     					// Change cycle 1.1 - change IN into inputData
+	private static library library; 						// Change cycle 1.2 - change LIB into library
+	private static String menu; 							// Change cycle 1.3 - change MENU into menu
+	private static Calendar calendar;						// Change cycle 1.4 - change CAL into calendar
+	private static SimpleDateFormat simpleDateFormat;		// Change cycle 1.5 - change SDF into simpleDateFormat
 	
-	
-	private static String Get_menu() {
-		StringBuilder sb = new StringBuilder();
+	//
+	private static String getMenu() {   					// Change cycle 1.6 - change Get_menu into getMenu
+		StringBuilder stringBuilder = new StringBuilder();	// Change cycle 1.7 - change sb into stringBuilder
 		
-		sb.append("\nLibrary Main Menu\n\n")
+		stringBuilder.append("\nLibrary Main Menu\n\n")		// Change cycle 1.8 - change sb into stringBuilder
 		  .append("  M  : add member\n")
 		  .append("  LM : list members\n")
 		  .append("\n")
@@ -33,33 +33,33 @@ public class Main {
 		  .append("\n")
 		  .append("Choice : ");
 		  
-		return sb.toString();
+		return stringBuilder.toString();					// Change cycle 1.9 - change sb into stringBuilder
 	}
 
 
 	public static void main(String[] args) {		
 		try {			
-			IN = new Scanner(System.in);
-			LIB = library.INSTANCE();
-			CAL = Calendar.getInstance();
-			SDF = new SimpleDateFormat("dd/MM/yyyy");
+			inputData = new Scanner(System.in);         	// Change cycle 1.10 - change IN into inputData
+			library = library.INSTANCE();						// Change cycle 1.11 - change LIB into library
+			calendar = Calendar.getInstance();					// Change cycle 1.12 - change CAL into calendar
+			simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy");		// Change cycle 1.13 - change SDF into simpleDataFormat
 	
-			for (member m : LIB.Members()) {
+			for (member m : library.Members()) {				// Change cycle 1.14 - change LIB into library
 				output(m);
 			}
 			output(" ");
-			for (book b : LIB.Books()) {
+			for (book b : library.Books()) {					// Change cycle 1.15 - change LIB into library
 				output(b);
 			}
 						
-			MENU = Get_menu();
+			menu = getMenu();								//Change cycle 1.16 - change Get_menu into getMenu
 			
-			boolean e = false;
+			boolean isException = false;       				// Change cycle 1.17 - change e into isException
 			
-			while (!e) {
+			while (!isException) {							// Change cycle 1.18 - change e into isException
 				
-				output("\n" + SDF.format(CAL.Date()));
-				String c = input(MENU);
+				output("\n" + simpleDateFormat.format(calendar.Date()));		// Change cycle 1.19 - change SDF into simpleDatEFormat
+				String c = input(menu);											// Change cycle 1.20 - change MENU into menu
 				
 				switch (c.toUpperCase()) {
 				
@@ -104,7 +104,7 @@ public class Main {
 					break;
 					
 				case "Q": 
-					e = true;
+					isException = true;						// Change cycle 1.21 - change e into isException
 					break;
 					
 				default: 
@@ -112,10 +112,10 @@ public class Main {
 					break;
 				}
 				
-				library.SAVE();
+				library.SAVE();								// Change cycle 1.22 - change LIB into library
 			}			
-		} catch (RuntimeException e) {
-			output(e);
+		} catch (RuntimeException isException) {			// Change cycle 1.23 - change e into isException
+			output(isException);							// Change cycle 1.24 - change e into isException
 		}		
 		output("\nEnded\n");
 	}	
@@ -127,7 +127,7 @@ public class Main {
 
 	private static void listCurrentLoans() {
 		output("");
-		for (loan loan : LIB.CurrentLoans()) {
+		for (loan loan : library.CurrentLoans()) {			// Change cycle 1.25 - change LIB into library
 			output(loan + "\n");
 		}		
 	}
@@ -136,7 +136,7 @@ public class Main {
 
 	private static void listBooks() {
 		output("");
-		for (book book : LIB.Books()) {
+		for (book book : library.Books()) {
 			output(book + "\n");
 		}		
 	}
@@ -145,7 +145,7 @@ public class Main {
 
 	private static void listMembers() {
 		output("");
-		for (member member : LIB.Members()) {
+		for (member member : library.Members()) {
 			output(member + "\n");
 		}		
 	}
@@ -170,11 +170,11 @@ public class Main {
 	private static void incrementDate() {
 		try {
 			int days = Integer.valueOf(input("Enter number of days: ")).intValue();
-			CAL.incrementDate(days);
-			LIB.checkCurrentLoans();
-			output(SDF.format(CAL.Date()));
+			calendar.incrementDate(days);
+			library.checkCurrentLoans();
+			output(simpleDateFormat.format(calendar.Date()));
 			
-		} catch (NumberFormatException e) {
+		} catch (NumberFormatException isException) {
 			 output("\nInvalid number of days\n");
 		}
 	}
@@ -185,7 +185,7 @@ public class Main {
 		String author = input("Enter author: ");
 		String title  = input("Enter title: ");
 		String callNo = input("Enter call number: ");
-		book book = LIB.Add_book(author, title, callNo);
+		book book = library.Add_book(author, title, callNo);
 		output("\n" + book + "\n");
 		
 	}
@@ -197,10 +197,10 @@ public class Main {
 			String firstName  = input("Enter first name: ");
 			String email = input("Enter email: ");
 			int phoneNo = Integer.valueOf(input("Enter phone number: ")).intValue();
-			member member = LIB.Add_mem(lastName, firstName, email, phoneNo);
+			member member = library.Add_mem(lastName, firstName, email, phoneNo);
 			output("\n" + member + "\n");
 			
-		} catch (NumberFormatException e) {
+		} catch (NumberFormatException isException) {
 			 output("\nInvalid phone number\n");
 		}
 		
@@ -209,7 +209,7 @@ public class Main {
 
 	private static String input(String prompt) {
 		System.out.print(prompt);
-		return IN.nextLine();
+		return inputData.nextLine();
 	}
 	
 	
