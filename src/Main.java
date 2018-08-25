c9dfe4afbb21b56451166209c162604aedc3531c
hashini <hashiniuttara@ymail.com>
2018-08-25 19:17:44 +1000

    Changes_after_review
    worked on review codes 1.1 -1.11

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,9 @@ import java.util.Scanner;
 public class Main {
 	
 	private static Scanner inputData;     	// Change cycle 1.1 - change IN into inputData
-	private static library library; 	// Change cycle 1.2 - change LIB into library       
+	private static Library library; 	// Change cycle 1.2 - change LIB into library       
 						// Review_1.1 Object class library (First library after static) should be as Library
+						// 1.1 Review done change library class into Library
 	private static String menu; 							// Change cycle 1.3 - change MENU into menu
 	private static Calendar calendar;						// Change cycle 1.4 - change CAL into calendar
 	private static SimpleDateFormat simpleDateFormat;		// Change cycle 1.5 - change SDF into simpleDateFormat
@@ -41,19 +42,24 @@ public class Main {
 	public static void main(String[] args) {		
 		try {			
 			inputData = new Scanner(System.in);         	// Change cycle 1.10 - change IN into inputData
-			library = library.INSTANCE();			// Change cycle 1.11 - change LIB into library    
+			library = Library.INSTANCE();			// Change cycle 1.11 - change LIB into library    
 									// Review_1.2 library.INSTANCE has to be changed to Library.INSTANCE
+									//1.2 Review done change library.INSTANCE into Library.INSTANCE
 			calendar = Calendar.getInstance();					// Change cycle 1.12 - change CAL into calendar
 			simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy");		// Change cycle 1.13 - change SDF into simpleDataFormat
 	
-			for (member m : library.Members()) {	// Change cycle 1.14 - change LIB into library    
+			for (Member member : library.Members()) {	// Change cycle 1.14 - change LIB into library    
 								// Review_1.3 member is a Class object. It has to be Member and the variable m has to be member.
-				output(m);			// Review_1.4 Input parameter variable m has to be member
+								//1.3 review done change member into Member and m into member
+				output(member);			// Review_1.4 Input parameter variable m has to be member
+										// 	1.4 review done change  m into member
 			}
 			output(" ");
-			for (book b : library.Books()) {	// Change cycle 1.15 - change LIB into library
+			for (Book book : library.Books()) {	// Change cycle 1.15 - change LIB into library
 								// Review_1.5 book is a Class object. It has to be Book and the variable b has to be book.
-				output(b);			// Review_1.6 Input parameter variable b has to be book
+								//1.5 review done change book into Book and b into book
+				output(book);			// Review_1.6 Input parameter variable b has to be book
+									//1.6 review done change b into book
 			}
 						
 			menu = getMenu();								//Change cycle 1.16 - change Get_menu into getMenu
@@ -131,8 +137,9 @@ public class Main {
 
 	private static void listCurrentLoans() {
 		output("");
-		for (loan loan : library.CurrentLoans()) {		// Change cycle 1.25 - change LIB into library
+		for (Loan loan : library.CurrentLoans()) {		// Change cycle 1.25 - change LIB into library
 									// Review_1.7 loan is a Class object. It has to be Loan.
+									// 1.7 review done change class object loan into Loan 
 			output(loan + "\n");
 		}		
 	}
@@ -141,7 +148,8 @@ public class Main {
 
 	private static void listBooks() {
 		output("");
-		for (book book : library.Books()) {   // Review_1.8 book is a Class object. It has to be Book.
+		for (Book book : library.Books()) {   // Review_1.8 book is a Class object. It has to be Book.
+											// 1.8 review done change class object book into Book
 			output(book + "\n");
 		}		
 	}
@@ -150,7 +158,8 @@ public class Main {
 
 	private static void listMembers() {
 		output("");
-		for (member member : library.Members()) {  // Review_1.9 member is a Class object. It has to be Member.
+		for (Member member : library.Members()) {  // Review_1.9 member is a Class object. It has to be Member.
+													// 1.9 review done change class object member into Member
 			output(member + "\n");
 		}		
 	}
@@ -190,7 +199,8 @@ public class Main {
 		String author = input("Enter author: ");
 		String title  = input("Enter title: ");
 		String callNo = input("Enter call number: ");
-		book book = library.Add_book(author, title, callNo); // Review_1.10 book is a Class object. It has to be Book (First book).
+		Book book = library.Add_book(author, title, callNo); // Review_1.10 book is a Class object. It has to be Book (First book).
+		                                                     //1.10 review done change  class object book into Book
 		output("\n" + book + "\n");
 		
 	}
@@ -202,7 +212,8 @@ public class Main {
 			String firstName  = input("Enter first name: ");
 			String email = input("Enter email: ");
 			int phoneNo = Integer.valueOf(input("Enter phone number: ")).intValue();
-			member member = library.Add_mem(lastName, firstName, email, phoneNo);  // Review_1.11 member is a Class object. It has to be Member.
+			Member member = library.Add_mem(lastName, firstName, email, phoneNo);  // Review_1.11 member is a Class object. It has to be Member.
+			                                                                    //1.11 review done Change member class object into Member
 			output("\n" + member + "\n");
 			
 		} catch (NumberFormatException isException) {
