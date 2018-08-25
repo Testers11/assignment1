e4deb1334240a3bf4040ecea52820967d1f9b578
Malinda Obeysekara <malinda_@live.co.uk>
2018-08-26 02:10:23 +1000

    PayFineControl.java changes cycle 2
    changes _m_1.01,m_1.02,m_1.03,m_1.04,m_1.05,m_1.06,m_1.07,m_1.08

--- src/PayFineControl.java
+++ src/PayFineControl.java
@@ -1,31 +1,29 @@
-
 /**
+ * change_m_1.01
  * author : Malinda Obaysekara
  * Provide the set of functions which related to paying fine
  */
-
-
 public class PayFineControl {
 	
-	private PayFineUI ui;	//change_m_1.01 Variable name has to change 
-	private ControlStateEnum state;	//change_m_1.02 Variable name has to change
-	private Library library;  //change_m_1.03 Variable name has to change
-	private Member member;    //change_m_1.04 Variable name has to change
 	
+	// change_m_1.02 defining variables, that variables used for initial state of class
 	private PayFineUI ui;
+	// change_m_1.03enumeration for each state of Control class
 	private enum CONTROL_STATE { INITIALISED, READY, PAYING, COMPLETED, CANCELLED };
-	private CONTROL_STATE state;
-	private library library;
-	private member member;;
+	private CONTROL_STATE state;	
+	private Library library;
+	private Member member;;
 
 
+	// change_m_1.04 setup initial settings for the PayFineControl instance 
 	public PayFineControl() {
-		this.library = library.INSTANCE();
+		this.library = Library.createInstance();
 		state = CONTROL_STATE.INITIALISED;
 	}
 	
-	
+	// change_m_1.05 inject PayFineUI Instance to PayFineControl Class
 	public void setUI(PayFineUI ui) {
+		// change_m_1.05 check for status is in initialized or not
 		if (!state.equals(CONTROL_STATE.INITIALISED)) {
 			throw new RuntimeException("PayFineControl: cannot call setUI except in INITIALISED state");
 		}	
@@ -34,8 +32,9 @@ public class PayFineControl {
 		state = CONTROL_STATE.READY;		
 	}
 
-
+	// change_m_1.06 verifying the member by passing member id
 	public void cardSwiped(int memberId) {
+		// check for status is in READY or not
 		if (!state.equals(CONTROL_STATE.READY)) {
 			throw new RuntimeException("PayFineControl: cannot call cardSwiped except in READY state");
 		}	
@@ -50,13 +49,13 @@ public class PayFineControl {
 		state = CONTROL_STATE.PAYING;
 	}
 	
-	
+	// change_m_1.07 handle the status for cancel UI instance and control class 
 	public void cancel() {
 		ui.setState(PayFineUI.UI_STATE.CANCELLED);
 		state = CONTROL_STATE.CANCELLED;
 	}
 
-
+	// change_m_1.08 handle payment by specifying amount & complete the process.
 	public double payFine(double amount) {
 		if (!state.equals(CONTROL_STATE.PAYING)) {
 			throw new RuntimeException("PayFineControl: cannot call payFine except in PAYING state");
