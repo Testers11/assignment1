e9306057c1a705448c65834a1977d0df28d00781
Testers11 <41605926+Testers11@users.noreply.github.com>
2018-08-25 18:14:05 +1000

    Review Main.java class by Mohamed Nashath
    Reviewed and need to do some correction. I have added review codes as easy to check. Do correcftions on review numbers from 1.1 to 1.11

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,14 @@ import java.util.Scanner;
 
 public class Main {
 	
-	private static Scanner inputData;     					// Change cycle 1.1 - change IN into inputData
-	private static library library; 						// Change cycle 1.2 - change LIB into library
+	private static Scanner inputData;     	// Change cycle 1.1 - change IN into inputData
+	private static library library; 	// Change cycle 1.2 - change LIB into library       
+						// Review_1.1 Object class library (First library after static) should be as Library
 	private static String menu; 							// Change cycle 1.3 - change MENU into menu
 	private static Calendar calendar;						// Change cycle 1.4 - change CAL into calendar
 	private static SimpleDateFormat simpleDateFormat;		// Change cycle 1.5 - change SDF into simpleDateFormat
 	
-	//
+	
 	private static String getMenu() {   					// Change cycle 1.6 - change Get_menu into getMenu
 		StringBuilder stringBuilder = new StringBuilder();	// Change cycle 1.7 - change sb into stringBuilder
 		
@@ -40,16 +41,19 @@ public class Main {
 	public static void main(String[] args) {		
 		try {			
 			inputData = new Scanner(System.in);         	// Change cycle 1.10 - change IN into inputData
-			library = library.INSTANCE();						// Change cycle 1.11 - change LIB into library
+			library = library.INSTANCE();			// Change cycle 1.11 - change LIB into library    
+									// Review_1.2 library.INSTANCE has to be changed to Library.INSTANCE
 			calendar = Calendar.getInstance();					// Change cycle 1.12 - change CAL into calendar
 			simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy");		// Change cycle 1.13 - change SDF into simpleDataFormat
 	
-			for (member m : library.Members()) {				// Change cycle 1.14 - change LIB into library
-				output(m);
+			for (member m : library.Members()) {	// Change cycle 1.14 - change LIB into library    
+								// Review_1.3 member is a Class object. It has to be Member and the variable m has to be member.
+				output(m);			// Review_1.4 Input parameter variable m has to be member
 			}
 			output(" ");
-			for (book b : library.Books()) {					// Change cycle 1.15 - change LIB into library
-				output(b);
+			for (book b : library.Books()) {	// Change cycle 1.15 - change LIB into library
+								// Review_1.5 book is a Class object. It has to be Book and the variable b has to be book.
+				output(b);			// Review_1.6 Input parameter variable b has to be book
 			}
 						
 			menu = getMenu();								//Change cycle 1.16 - change Get_menu into getMenu
@@ -59,7 +63,7 @@ public class Main {
 			while (!isException) {							// Change cycle 1.18 - change e into isException
 				
 				output("\n" + simpleDateFormat.format(calendar.Date()));		// Change cycle 1.19 - change SDF into simpleDatEFormat
-				String c = input(menu);											// Change cycle 1.20 - change MENU into menu
+				String c = input(menu);		// Change cycle 1.20 - change MENU into menu
 				
 				switch (c.toUpperCase()) {
 				
@@ -127,7 +131,8 @@ public class Main {
 
 	private static void listCurrentLoans() {
 		output("");
-		for (loan loan : library.CurrentLoans()) {			// Change cycle 1.25 - change LIB into library
+		for (loan loan : library.CurrentLoans()) {		// Change cycle 1.25 - change LIB into library
+									// Review_1.7 loan is a Class object. It has to be Loan.
 			output(loan + "\n");
 		}		
 	}
@@ -136,7 +141,7 @@ public class Main {
 
 	private static void listBooks() {
 		output("");
-		for (book book : library.Books()) {
+		for (book book : library.Books()) {   // Review_1.8 book is a Class object. It has to be Book.
 			output(book + "\n");
 		}		
 	}
@@ -145,7 +150,7 @@ public class Main {
 
 	private static void listMembers() {
 		output("");
-		for (member member : library.Members()) {
+		for (member member : library.Members()) {  // Review_1.9 member is a Class object. It has to be Member.
 			output(member + "\n");
 		}		
 	}
@@ -185,7 +190,7 @@ public class Main {
 		String author = input("Enter author: ");
 		String title  = input("Enter title: ");
 		String callNo = input("Enter call number: ");
-		book book = library.Add_book(author, title, callNo);
+		book book = library.Add_book(author, title, callNo); // Review_1.10 book is a Class object. It has to be Book (First book).
 		output("\n" + book + "\n");
 		
 	}
@@ -197,7 +202,7 @@ public class Main {
 			String firstName  = input("Enter first name: ");
 			String email = input("Enter email: ");
 			int phoneNo = Integer.valueOf(input("Enter phone number: ")).intValue();
-			member member = library.Add_mem(lastName, firstName, email, phoneNo);
+			member member = library.Add_mem(lastName, firstName, email, phoneNo);  // Review_1.11 member is a Class object. It has to be Member.
 			output("\n" + member + "\n");
 			
 		} catch (NumberFormatException isException) {
